4525df9154f140a865b3c7b180830b2b7ba77e4c
nikumoradi <nikumoradi@gamail.com>
2023-08-16 14:25:30 +0330

    Complete Btn  Added

--- 7 - ToDoList-LocalStorage/js.js
+++ 7 - ToDoList-LocalStorage/js.js
@@ -18,92 +18,152 @@ let clearBtn = _qs('.clear-btn')
 let itemsUl = _qs('.items')
 
 
-let tasksArray = []
-
-
-function generatore() {
-
-    let newTaskTitle = todoInput.value
-    let taskObject = {
-        id: tasksArray.length + 1,
-        taskTitle: newTaskTitle,
+let todosArray = []
+//add todo btn
+function addNewTodo() {
+    let newTodoTitle = todoInput.value
+    let newTodoObj = {
+        id: todosArray.length + 1,
+        title: newTodoTitle,
         complete: false
     }
-    tasksArray.push(taskObject)
-    setArrayinLocalStorage(tasksArray);
-    tasksObjGenerator(tasksArray)
-
-
-
-    function setArrayinLocalStorage(array) {
-        localStorage.setItem('taskArray', JSON.stringify(array))
-    }
-
-    function tasksObjGenerator(array) {
-        array.forEach(function (obj) {
-            //li
-            let newLi = $.createElement('li')
-            newLi.classList.add('item')
-
-            //li btns
-            let newBtnsDiv = $.createElement('div')
-            newBtnsDiv.classList.add('li-btns')
-
-            let newStatusBtn = $.createElement('input')
-            newStatusBtn.classList.add('status-btn')
-            newStatusBtn.setAttribute('type', 'submit')
-            newStatusBtn.value = 'Complete'
-
-            let newDeleteBtn = $.createElement('input')
-            newDeleteBtn.classList.add('delete-btn')
-            newDeleteBtn.setAttribute('type', 'submit')
-            newDeleteBtn.value = 'Delete'
-
-            //span
-            let newTodoSpan = $.createElement('span')
-            newTodoSpan.classList.add('span')
-            newTodoSpan.innerHTML = obj.taskTitle
-            //apppneds
-            newBtnsDiv.append(newStatusBtn, newDeleteBtn)
-            newLi.append(newTodoSpan, newBtnsDiv)
-            itemsUl.append(newLi)
-            todoInput.value = ''
-
-        })
-
-    }
+    todosArray.push(newTodoObj)
+    setLocalStorage(todosArray)
+    todosGenerator(todosArray)
+    todoInput.focus()
 
 }
-
-// function getArrayfromLocalStorage(){
-//     let localStorageTasks = JSON.parse(localStorage.getItem('taskArray'))
-//     console.log(localStorageTasks)
-//     if(localStorageTasks){
-//         tasksArray = localStorageTasks
-//     }else{
-//         tasksArray = []
-//     }
-//     generatore(tasksArray)
-// }
-function addToListHandlerByClick() {
-    generatore()
-}
-
-function addToListHandlerByEnter(event) {
+function addNewTodoByClick(event) {
     if (event.key === 'Enter') {
-       generatore();
+        addNewTodo();
     }
 }
-// function clearAll(){
-//     tasksArray=[]
-//     tasksObjGenerator[tasksArray]
-//     localStorage.clear()
-// }
-//eventListeners
-addBtn.addEventListener('click', addToListHandlerByClick)
-$.body.addEventListener('keydown', addToListHandlerByEnter)
-// clearBtn.addEventListener('click',clearAll)
+//clear todo list btn
+function clearAll() {
+    todosArray = []
+    itemsUl.innerHTML = ''
+    localStorage.removeItem('todos')
+}
 
+
+//delete btn
+function removeTodo(todoId) {
+    localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+    todosArray = localStorageTodos
+    let selectedTodoIndex = todosArray.findIndex(function (todo) {
+        return todo.id === todoId
+    })
+    todosArray.splice(selectedTodoIndex, 1)
+    setLocalStorage(todosArray)
+    todosGenerator(todosArray)
+}
+//complete btn
+function changeStatusTodo(todoId) {
+    localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+    todosArray = localStorageTodos
+    todosArray.forEach(function (todo) {
+        if (todo.id === todoId) {
+            todo.complete = !todo.complete
+        }
+    })
+    setLocalStorage(todosArray)
+    todosGenerator(todosArray)
+}
+//localStorage
+function setLocalStorage(todosList) {
+    localStorage.setItem('todos', JSON.stringify(todosList))
+}
+
+function getLocalStorage() {
+    let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+    if (localStorageTodos) {
+        todosGenerator(localStorageTodos)
+    }
+}
+//Generator
+function todosGenerator(todosList) {
+    itemsUl.innerHTML = ''
+    todosList.forEach(function (todo) {
+        let newLi = $.createElement('li')
+        newLi.classList.add('item')
+
+        let newBtnsDiv = $.createElement('div')
+        newBtnsDiv.classList.add('li-btns')
+
+        let newStatusBtn = $.createElement('input')
+        newStatusBtn.classList.add('status-btn')
+        newStatusBtn.setAttribute('type', 'submit')
+        newStatusBtn.value = 'Complete'
+        newStatusBtn.setAttribute('onclick', 'changeStatusTodo(' + todo.id + ')')
+
+        if (todo.complete) {
+            newStatusBtn.value = 'UnCompleted'
+            newStatusBtn.classList += ' completed'
+        }
+
+        let newDeleteBtn = $.createElement('input')
+        newDeleteBtn.classList.add('delete-btn')
+        newDeleteBtn.setAttribute('type', 'submit')
+        newDeleteBtn.value = 'Delete'
+        newDeleteBtn.setAttribute('onclick', 'removeTodo(' + todo.id + ')')
+
+        let newTodoSpan = $.createElement('span')
+        newTodoSpan.classList.add('span')
+        newTodoSpan.innerHTML = todo.title
+
+        newBtnsDiv.append(newStatusBtn, newDeleteBtn)
+        newLi.append(newTodoSpan, newBtnsDiv)
+        itemsUl.append(newLi)
+
+        todoInput.value = ''
+    })
+}
+
+
+
+
+
+window.addEventListener('load', getLocalStorage)
+addBtn.addEventListener('click', addNewTodo)
+clearBtn.addEventListener('click', clearAll)
+todoInput.addEventListener('keydown', addNewTodoByClick)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getLocalStorage() {
+//     let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+//     if (localStorageTodos) {
+//         todosArray = localStorageTodos
+//     } else {
+//         todosArray = []
+//     }
+//     todosGenerator(todosArray)
+// }
 
 
 
